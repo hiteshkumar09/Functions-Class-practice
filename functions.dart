9d3e9f693c647a9ef85a748fedde34dc076642a0
hiteshkumar09 <hitesh.kumarmalhi09@gmail.com>
2025-01-03 10:05:26 +0500

    functions Practice

---
+++ functions.dart
@@ -0,0 +1,59 @@
+// ///--- Functions in Dart ---////
+// ///--- Function void main ma kabi nh bana ga but just use hoga cal hoga jhan function cal hoga tou us ka 
+// ///output ai ga asa bana ke rakh diya tou kuch bhe nhe hoga
+// ///---function ke ander line of code store hota hai
+
+
+// void main(){
+//      // Students
+//      print("For Students");
+//     marksheet();
+
+//      // Teachers
+//      print("For Teachers");
+//     marksheet();
+
+//      // Parents
+//      print("For Parents");
+//     marksheet();
+// }
+
+// marksheet(){ //// carlly bracket ma  huamri body hoti hai  
+//     // Students
+//     print("Hitesh");
+//     num percentage = 43;
+//     if (percentage >= 50) {
+//       print("Pass");
+//     }else{
+//         print("Fail");
+//     }
+// }
+
+
+
+
+///--- Functions in Dart ---////
+///--- Function void main ma kabi nh bana ga but just use hoga cal hoga jhan function cal hoga tou us ka 
+///output ai ga asa bana ke rakh diya tou kuch bhe nhe hoga
+///---function ke ander line of code store hota hai
+/// 
+
+void main(){
+    marksheet("Hitesh", 34);
+    marksheet("Huzaifa", 3);
+    marksheet("Kamlesh", 34);
+    marksheet("Ali Raza", 3);
+    marksheet("Doulat", 34);
+    
+}
+
+marksheet(String name, num percentage){ //// carlly bracket ma  huamri body hoti hai  
+    // Students
+    print(name);
+    
+    if (percentage >= 50) {
+      print("Pass");
+    }else{
+        print("Fail");
+    }
+}
