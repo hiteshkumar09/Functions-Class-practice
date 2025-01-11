b145d4a9ae82835cd67001c784b1771a8428d852
hiteshkumar09 <hitesh.kumarmalhi09@gmail.com>
2025-01-11 18:28:54 +0500

    functions Practice

--- function3.dart
+++ function3.dart
@@ -1,18 +1,12 @@
 ///--- Functions in Dart ---////
-///--- Function void main ma kabi nh bana ga but just use hoga cal hoga jhan function cal hoga tou us ka 
-///output ai ga asa bana ke rakh diya tou kuch bhe nhe hoga
-///---function ke ander line of code store hota hai
-/// 
-
+// Tou Optional parameter hum log iss condition ma use karte hain
 void main(){
     marksheet(name : "Hitesh", percentage: 99);
     marksheet(name : "Doulat", percentage: 1);
     marksheet(name : "ABC", percentage: 55, sec: "B");
     marksheet(name : "Mahesh", percentage: 77);
-    
-    
-}
-
+  }
+// section optional hai
 marksheet({required  name, required num percentage, String sec = "A"}){ //// carlly bracket ma  huamri body hoti hai  
     // Students
     print(name);
