4f712463c1895db1c7fa5ff858dae5f87fbe9b79
hiteshkumar09 <hitesh.kumarmalhi09@gmail.com>
2025-01-26 12:52:35 +0500

    functions Practice

--- functions4-Types&NullSafety.dart
+++ functions4-Types&NullSafety.dart
@@ -1,4 +1,14 @@
 // -----There Are Two Types OF functions 
 //----(01) Void Type
 //-----(02) Return Type
-void main() {}
+void main() {
+  var abc = sumOfTwoNumbers();
+  print("Value of abc : $abc");
+}
+
+sumOfTwoNumbers(){
+  int num1 = 6;
+  int num2 = 8;
+  int result = num1 + num2;
+  print("Value of result : $result");
+}
